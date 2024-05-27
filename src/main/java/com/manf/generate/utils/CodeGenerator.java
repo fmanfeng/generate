81c2e0da632a256a4665b688df31aed0b0f45b8b
fmanfeng <fu978177136@163.com>
2024-05-27 15:31:03 +0800

    已升级版本

--- src/main/java/com/manf/generate/utils/CodeGenerator.java
+++ src/main/java/com/manf/generate/utils/CodeGenerator.java
@@ -1,8 +1,11 @@
 package com.manf.generate.utils;
 
 
+
+
 import com.baomidou.mybatisplus.generator.FastAutoGenerator;
 import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
 import java.util.Collections;
 
@@ -23,13 +26,13 @@ public class CodeGenerator {
                 .globalConfig(builder -> {
                     builder.author("manfeng") // 设置作者
                             .enableSwagger() // 开启 swagger 模式
-                            .fileOverride() // 覆盖已生成文件
                             .outputDir("D:\\JavaProject\\generate\\src\\main\\java\\"); // 指定输出目录
+
                 })
                 .packageConfig(builder -> {
                     builder.parent("com.manf.generate") // 设置父包名
                             .moduleName(null) // 设置父包模块名 没有为空
-                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "D:\\JavaProject\\generate\\src\\main\\resources\\mapper")); // 设置mapperXml生成路径
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, "D:\\JavaProject\\generate\\src\\main\\resources\\mapper")); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
                     builder.entityBuilder().enableLombok();
@@ -41,7 +44,7 @@ public class CodeGenerator {
                             .addTablePrefix("t_", "c_"); // 设置过滤表前缀
 
                 })
-                //.templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
                 .execute();
 
     }
